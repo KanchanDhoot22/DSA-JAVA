655aaa477463cebc532e0d3c8f4fa882eeb5d3e6
Kanchan <kanchandhoot3@gmail.com>
2021-09-20 23:49:37 +0530

    1st commit

--- src/com/company/linearSearch.java
+++ src/com/company/linearSearch.java
@@ -3,35 +3,67 @@ package com.company;
 public class linearSearch {
 
     public static void main(String[] args) {
-        int [] nums ={23 , 56 , 88 , 99 , 45 , -1 , -42};
-        int target = 45;
-        int ans =linearSearch(nums , target);
-        System.out.println(ans);
+      int [] arr = {22 , 34 , 19 , 45 , 99 , 11};
+       int target = 45;
+      boolean ans = linearSearch3(arr,target);
+       System.out.println(ans);
+
+//    SEARCH THE ELEMENT AND RETURN TRUE OR FALSE.
 
     }
-//    searching in the array : return the index if item found.
-//    otherwise if itm not found return -1.
-    static int linearSearch (int [] arr , int target){
+
+    static boolean linearSearch3(int[]arr ,int target){
         if (arr.length==0){
+            return false;
+        }
+//       run a for loop
+        for (int element : arr){
+            if (element==target){
+                return true;
+            }
+        }
+//     this line will execute if none of the return statements above have executed
+//       hence the target not found
+       return false;
+    }
+
+
+
+//    SEARCH THE TARGET AND RETURN THE ELEMENT.
+    static int linearsearch2(int[]arr,int target){
+        if(arr.length==0){
             return -1;
         }
 //        run a for loop
-        for (int index=0; index<arr.length; index++){
-//            check for element at every index if element = target
-            int element = arr[index];
+        for (int element : arr){
             if (element==target){
-                return index;
+                System.out.println(element);
             }
-
-
         }
-//        this line will execute if none of the above return statement works.
-//        hence target not found.
-        return -1;
-
-
-
+        // this line will execute if none of the return statements above have executed
+        // hence the target not found
+        return Integer.MAX_VALUE;
     }
+
+//    SEARCH THE TARGET AND RETURN ITS INDEX
+static int linearSearch(int[]arr,int target){
+        if(arr.length==0){
+            return -1;
+        }
+    //        run a for loop
+    for (int index = 0 ; index< arr.length; index++){
+        int element = arr[index];
+        if (element==target){
+            return index;
+        }
+    }
+    // this line will execute if none of the return statements above have executed
+    // hence the target not found
+    return -1;
+
+
+}
+
 
 
 

4f40a94c80ab3ab9a9b188a69358a2ee57042d4a
Kanchan Dhoot <88830850+KanchanDhoot22@users.noreply.github.com>
2022-02-05 18:58:43 +0530

    one file added

---
+++ Pattern_Questions/patternQ1.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class patternQ1 {
+
+    static void pattern1(int n){
+        for (int row=0; row<= n; row++){
+            // for every row , run the col
+            for (int col = 1; col<= row; col++){
+                System.out.print("* ");
+            }
+            // when one row is printed , we need to add a new line.
+            System.out.println();
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        pattern1(4);
+    }
+}
